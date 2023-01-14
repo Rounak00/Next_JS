8e39a73b29a82ba9265cf8590ab3ee68a955abf8
Rounak00 <mukherjeerounak10@gmail.com>
2023-01-14 14:14:07 +0530

    git push

--- ourapp/pages/index.js
+++ ourapp/pages/index.js
@@ -1,14 +1,24 @@
 
 
-import Link from "next/link";
+import Image from "next/image";
 import Nav from "@/components/Nav";
+
 const index= ()=> {
   return (
     <>
     <Nav />
       <p>Hello </p> 
+      <Image src="/pic.jpg" height="20" width="20"></Image> { /* add height width or layout  */}
     </>
   );
 };
 export default index;
 // this index go to app.js and return there as component
+
+
+//for adding online image link we need to write code in next.config.js
+/*
+ images:{
+   domains:["doman name"] //example ["image.pixels.com"]
+ }
+*/
