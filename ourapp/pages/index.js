fca7cefb65589b2123598c99d34657c11792aa52
Rounak00 <mukherjeerounak10@gmail.com>
2023-01-14 20:34:47 +0530

    git push

--- ourapp/pages/index.js
+++ ourapp/pages/index.js
@@ -2,10 +2,13 @@
 
 import Image from "next/image";
 import Nav from "@/components/Nav";
-
+import Head from "next/head";
 const index= ()=> {
   return (
     <>
+    <Head>  {/*// head tag in next use for meta and title */}
+      <title> First open</title>
+    </Head>
     <Nav />
       <p>Hello </p> 
       <Image src="/pic.jpg" height="20" width="20"></Image> { /* add height width or layout  */}
